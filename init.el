2377bf4269fdc9a7c2f1b592176a13b565276dc6
Jiamin Xu <jiamin.xu@agi-aero.com>
2021-01-16 16:43:39 +0800

    sync from macos

--- init.el
+++ init.el
@@ -111,13 +111,16 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("e1ef2d5b8091f4953fe17b4ca3dd143d476c106e221d92ded38614266cea3c8b" "d74c5485d42ca4b7f3092e50db687600d0e16006d8fa335c69cf4f379dbd0eee" "2a749c20af891c16571527d07976bbcf2bf31819fa7d322942b73386019f4d58" "9efb2d10bfb38fe7cd4586afb3e644d082cbcdb7435f3d1e8dd9413cbe5e61fc" default))
+   (quote
+    ("7a994c16aa550678846e82edc8c9d6a7d39cc6564baaaacc305a3fdc0bd8725f" "e1ef2d5b8091f4953fe17b4ca3dd143d476c106e221d92ded38614266cea3c8b" "d74c5485d42ca4b7f3092e50db687600d0e16006d8fa335c69cf4f379dbd0eee" "2a749c20af891c16571527d07976bbcf2bf31819fa7d322942b73386019f4d58" "9efb2d10bfb38fe7cd4586afb3e644d082cbcdb7435f3d1e8dd9413cbe5e61fc" default)))
  '(package-selected-packages
-   '(dap-mode elpy clang-format company-c-headers helm-gtags helm-projectile helm-swoop helm cmake-ide htmlize counsel-projectile projectile swiper smartparens info-colors doom-modeline doom-themes ace-window use-package))
- '(powerline-default-separator 'curve)
+   (quote
+    (dap-mode elpy clang-format company-c-headers helm-gtags helm-projectile helm-swoop helm cmake-ide htmlize counsel-projectile projectile swiper smartparens info-colors doom-modeline doom-themes ace-window use-package)))
+ '(powerline-default-separator (quote curve))
  '(powerline-gui-use-vcs-glyph t)
  '(safe-local-variable-values
-   '((company-clang-arguments "-I~/Desktop/ATHENA/src/" "-I~/Desktop/ATHENA/src/cfd/" "-I~/Desktop/ATHENA/src/common/" "-I~/Desktop/ATHENA/src/geometry/" "-I~/Desktop/ATHENA/src/material/" "-I~/Desktop/ATHENA/src/template/" "-I~/Desktop/ATHENA/src/utilities/" "-I~/Desktop/ATHENA/src/testsuite/" "-I~/Desktop/ATHENA/src/boost/boost/"))))
+   (quote
+    ((company-clang-arguments "-I~/Desktop/ATHENA/src/" "-I~/Desktop/ATHENA/src/cfd/" "-I~/Desktop/ATHENA/src/common/" "-I~/Desktop/ATHENA/src/geometry/" "-I~/Desktop/ATHENA/src/material/" "-I~/Desktop/ATHENA/src/template/" "-I~/Desktop/ATHENA/src/utilities/" "-I~/Desktop/ATHENA/src/testsuite/" "-I~/Desktop/ATHENA/src/boost/boost/")))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
