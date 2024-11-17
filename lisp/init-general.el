b9d3273ba2ad349971ec1a81fefa41fa8c1d1735
Jiamin Xu <jiamin.xu@live.com>
2024-11-17 21:00:07 +0800

    minor changes

--- lisp/init-general.el
+++ lisp/init-general.el
@@ -107,23 +107,20 @@
                                     dap-tooltip-mouse-motion
                                     gud-tooltip-mouse-motion)))
 ;; setup helm package
-;;(if (display-graphic-p)
-    (use-package helm
-      :ensure t
-      :init
-      (progn
-;;        (require 'helm-grep)
-        (helm-mode 1)
-        ;; setup help local keys
-        (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
-        (define-key helm-map (kbd "C-i") 'helm-execute-persistent-action)
-        (define-key helm-map (kbd "C-z") 'helm-select-action)
-        ;; override global key binds
-        (global-set-key (kbd "C-x b") 'helm-buffers-list)
-        (global-set-key (kbd "C-x C-f") 'helm-find-files)
-        (global-set-key (kbd "M-x") 'helm-M-x)
-        ;; remap global key binds
-        (define-key global-map [remap find-tag] 'helm-etags-select)))
+(use-package helm
+  :ensure t
+  :init
+  (progn
+    (helm-mode 1)
+    ;; setup help local keys
+    (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action)
+    (define-key helm-map (kbd "C-z") 'helm-select-action)
+    ;; override global key binds
+    (global-set-key (kbd "C-x b") 'helm-buffers-list)
+    (global-set-key (kbd "C-x C-f") 'helm-find-files)
+    (global-set-key (kbd "M-x") 'helm-M-x)
+    ;; remap global key binds
+    (define-key global-map [remap find-tag] 'helm-etags-select)))
 
 
 (defvar hs-special-modes-alist
